150e0ae5c2afb7cec3f4110fc530e3f5f276a254
dieroser <devroser@gmail.com>
2020-06-13 23:43:37 +0200

    Testing countdown bar

---
+++ 201906/PointAndClick/Assets/Scripts/BarFilling.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarFilling : MonoBehaviour
+{
+    private Transform bar;
+
+    void Awake()
+    {
+        bar = transform.Find("Bar");
+    }
+
+    public void SetSize(float sizeNormalized){
+        bar.localScale = new Vector3(sizeNormalized,1f);
+    }
+    
+}
---
+++ 201906/PointAndClick/Assets/Scripts/CountdownBarAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountdownBarAnimation : MonoBehaviour
+{
+    public GameObject countdownBar;
+    private BarFilling barFilling;
+
+    void Start()
+    {
+        barFilling = countdownBar.GetComponent<BarFilling>();
+		barFilling.SetSize(.4f);
+    }
+    
+}
