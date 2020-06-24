83e7196ab940c2a6e90bcf3647e62a0d5c830912
unknown <devroser@gmail.com>
2020-06-24 21:08:08 +0200

    Adding winning goal

--- Courses/Block Breaker/Assets/Scripts/Block.cs
+++ Courses/Block Breaker/Assets/Scripts/Block.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+    
+    // Cached reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.countBreakebleBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
+        level.blockDestroyed();
     }
 }
---
+++ Courses/Block Breaker/Assets/Scripts/Level.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    // Parameters
+    [SerializeField] int breakableBlocks; // Serialize for debugging purposes
+
+    // Cached reference
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void countBreakebleBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void blockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+}
