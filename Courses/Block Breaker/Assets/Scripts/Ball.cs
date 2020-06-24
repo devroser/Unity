4d5dcdec66d7f61594756865468cf53d3609716f
unknown <devroser@gmail.com>
2020-06-24 17:56:04 +0200

    Adding a second level, remove friction and play audio randomly

--- Courses/Block Breaker/Assets/Scripts/Ball.cs
+++ Courses/Block Breaker/Assets/Scripts/Ball.cs
@@ -9,16 +9,21 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
 
     // state
     Vector2 paddleToBallVector;
     bool hasStarted;
+
+    // Cached component reference
+    AudioSource myAudioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         hasStarted = false;
         paddleToBallVector = transform.position - paddle.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -44,5 +49,15 @@ public class Ball : MonoBehaviour
     {
         Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip randomClip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(randomClip);
+        }
+            
     }
 }
